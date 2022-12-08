8e9ea1f1bc355daec514d963d62e369f512e5ada
liuyupei <gerrydushkinktw19@gmail.com>
2022-12-09 01:21:14 +0800

    first commit

---
+++ src/components/OtherAbility.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>敬请期待...</h1>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  color: var(--vt-c-text-1);
+}
+
+</style>
